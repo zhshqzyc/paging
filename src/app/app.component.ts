446e86e03542b63cd51a7d9b85e070aac10880cf
Hui Zhao <zhshqzyc@gmail.com>
2024-05-15 10:51:53 -0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PagingComponent } from './paging/paging.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, PagingComponent],
 })
 export class AppComponent {
   title = 'paging';
+  totalItems: number = 5;
+  currentPage: number = 1;
+  step: number = 2;
+  pageSize: number = 25;
+
+  change(value: number) {
+    console.log(value);
+  }
 }
